091b48fa10fb2ae6649a8d6b2a8d6c2361f5d3b0
pgesieniec <przemyslaw.gesieniec@gmail.com>
2019-01-22 22:30:02 +0100

    init spring boot server, arduino draft

---
+++ server/saveKid/src/test/java/pl/project/saveKid/SaveKidApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.project.saveKid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaveKidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
