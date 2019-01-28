771c83b69702c9b3f34f31cf813b07e0c9692f45
pgesieniec <przemyslaw.gesieniec@gmail.com>
2019-01-28 21:38:46 +0100

    arduino part working

--- arduino/RfidGate/RfidGate.ino
+++ arduino/RfidGate/RfidGate.ino
@@ -1,4 +1,11 @@
 #include "WiFiEsp.h"
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 3
+#define RST_PIN 2
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
 char ssid[] = "DESKTOP_WIFI";      // your network SSID (name)
 char pass[] = "przemek123";        // your network password
@@ -12,6 +19,9 @@ WiFiEspClient client;
 
 void setup()
 {
+  SPI.begin();
+  rfid.PCD_Init();
+
   pinMode(pushButton, INPUT);
   Serial.begin(9600); // initialize serial for ESP module
   Serial1.begin(115200); // initialize ESP module
@@ -26,7 +36,7 @@ void setup()
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
   }
-  
+
   Serial.println("You're connected to the network");
   printWifiStatus();
   Serial.println();
@@ -34,11 +44,14 @@ void setup()
 
 }
 
-void loop()
-{
+void loop(){
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()){
+   
+    String rfidValue = getRfidValue();
+
     if (client.connect(server, 8090)) {
       Serial.println("Connected to server");
-      client.println("GET /get HTTP/1.1");
+      client.println("GET /get/"+rfidValue+" HTTP/1.1");
       client.println("Host: 192.168.137.1:8090");
       client.println("Connection: close");
       client.println();
@@ -46,16 +59,14 @@ void loop()
       Serial.println("message is sent");
     }
     delay(5000);
+  }
 }
-
 
 void printWifiStatus()
 {
-  // print the SSID of the network you're attached to
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
@@ -66,3 +77,21 @@ void printWifiStatus()
   Serial.print(rssi);
   Serial.println(" dBm");
 }
+
+String getRfidValue() {
+
+  String value = "";
+  value += rfid.uid.uidByte[0], HEX;
+  value += ":";
+  value += rfid.uid.uidByte[1], HEX;
+  value += ":";
+  value += rfid.uid.uidByte[2], HEX;
+  value += ":";
+  value += rfid.uid.uidByte[3], HEX;
+
+  Serial.println(value);
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+  return value;
+}
+
--- server/saveKid/src/main/java/pl/project/saveKid/controller/ArduinoRFIDController.java
+++ server/saveKid/src/main/java/pl/project/saveKid/controller/ArduinoRFIDController.java
@@ -4,16 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ArduinoRFIDController {
 
 
-    @GetMapping("/get")
+    @GetMapping("/get/{rfidId}")
     public @ResponseBody
-    ResponseEntity<String> getRFID() {
-        System.out.println("get");
+    ResponseEntity<String> getRFID(@PathVariable String rfidId) {
+        System.out.println("rfidId: " + rfidId);
         return new ResponseEntity<String>("GET Response", HttpStatus.OK);
     }
 }
